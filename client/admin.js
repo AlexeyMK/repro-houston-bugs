da3666f44fd4f36521da7ddb0f9832df22ece39d
Alexey Komissarouk <alexey@alexeymk.com>
2014-04-26 16:11:57 -0700

    got rid of some links

--- client/admin.js
+++ client/admin.js
@@ -11,10 +11,7 @@ Template.admin_powers.helpers({
 });
 
 Template.admin_powers.rendered = function() {
-  console.log("here and now2");
-  console.log($('.reset-scores'));
   $('.reset-scores').on('click', function() {
-    console.log("here and now");
     Entity.find().forEach(function(entity) {
       Entity.update(entity._id, {$set: {score: 0}});
     });
